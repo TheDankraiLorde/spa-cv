ad46af61b1c8be6560a6b31ba78ddc334be97562
TheDankraiLorde <menezesgaurav@gmail.com>
2020-01-16 00:58:11 +0530

    Started CV App

---
+++ src/data/data_val.js
@@ -0,0 +1,60 @@
+const data = {
+  name: "Gaurav Menezes",
+  about: `I am an IT professional of 2 and a half years of experience and I'm constantly looking to newer technologies to find ways to solve new problems or better ways to solve existing problems!`,
+  experience: {
+    years: 2,
+    months: 3
+  },
+  skills: [
+    {
+      react: {
+        time: {
+          years: 1,
+          months: 6
+        },
+        version: 16,
+        description:
+          "Learnt and have hands on React experience in creating components, both stateless and stateful, along with being able to consume APIs"
+      },
+      vue: {
+        time: {
+          years: 0,
+          months: 5
+        },
+        version: 2,
+        description:
+          "Proficient in state management using vuex, vue-resource and capable of using Axios for API fetching"
+      },
+      java: {
+        time: {
+          years: 2,
+          months: 5
+        },
+        version: 1.8,
+        description:
+          "I have knowledge on the basics of Java (Collections API, JDBC and Servlets) as well as hands on experience in performing enhancements in our project."
+      },
+      sql: {
+        time: {
+          years: 2,
+          months: 5
+        },
+        version: 0,
+        description:
+          "Can create custom queries to fetch the required data. Trained properly in SQL by current project."
+      },
+      python: {
+        time: {
+          years: 1,
+          months: 0
+        },
+        version: 3,
+        description:
+          "Learnt the basics of Python in project and am currently learning Flask."
+      }
+    }
+  ],
+  github: "https://github.com/TheDankraiLorde"
+};
+
+export default data;
